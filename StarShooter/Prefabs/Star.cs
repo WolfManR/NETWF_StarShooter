11187a1c7f179b9ea00df6836554c90f8811c54f
Tarxos <ftarxos@gmail.com>
2022-07-17 21:53:09 +0300

    add star prefab

---
+++ StarShooter/Prefabs/Star.cs
@@ -0,0 +1,28 @@
+﻿using StarShooter.GameEngine;
+
+namespace StarShooter.Prefabs;
+
+public class Star : GameObject
+{
+    public static Image Image { get; set; }
+
+    public Star(Point pos, Point dir, Size size) : base(pos, dir, size)
+    {
+        Log("Star created");
+    }
+    
+    public override void Draw(Graphics drawer)
+    {
+        drawer.DrawImage(Image, _position);
+
+        Log("Star drawn");
+    }
+
+    public override void Update()
+    {
+        _position.X += _direction.X;
+        if (_position.X < 0) _position.X = Configuration.WindowWidth + 20;
+
+        Log("Star updated");
+    }
+}
