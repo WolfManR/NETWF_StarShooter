2ac397d5f3426db53f716b68b9fd62fb397b5cec
Tarxos <ftarxos@gmail.com>
2022-07-17 18:07:50 +0300

    add Engine, Scene, and Timer

---
+++ StarShooter.GameEngine/Scene.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+
+namespace StarShooter.GameEngine;
+
+public abstract class Scene
+{
+    public List<GameObject> BackgroundObjects { get; set; }
+
+
+    public static void Test()
+    {
+        // Проверяем вывод графики
+        Engine.buffer.Graphics.Clear(Color.Black);
+        Engine.buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
+        Engine.buffer.Graphics.FillEllipse(Brushes.Wheat, new Rectangle(100, 100, 200, 200));
+    }
+
+    public abstract void Load();
+    public abstract void Draw();
+    public abstract void Update();
+}
---
+++ StarShooter.GameEngine/Timer.cs
@@ -0,0 +1,20 @@
+﻿namespace StarShooter.GameEngine;
+
+public class Timer
+{
+    private System.Threading.Timer? _timer;
+
+    public int Interval { get; set; } = 1000;
+
+    public event EventHandler<EventArgs>? Tick;
+
+    public void Start()
+    {
+        _timer ??= new(_ => { Tick?.Invoke(null, EventArgs.Empty); }, null, 0, Interval);
+    }
+
+    public void Stop()
+    {
+        _timer?.Dispose();
+    }
+}
