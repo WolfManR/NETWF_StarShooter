7a456cf5f696b71ca184cdc0cf01ce0b84db0a84
Tarxos <ftarxos@gmail.com>
2022-07-17 17:58:24 +0300

    ad game object

---
+++ StarShooter.GameEngine/GameObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+
+namespace StarShooter.GameEngine;
+
+public abstract class GameObject
+{
+    protected Point _position = new();
+    protected Point _direction = new();
+    protected Size _size = new();
+
+    protected GameObject() { }
+
+    protected GameObject(Point position, Point direction, Size size)
+    {
+        _position = position;
+        _direction = direction;
+        _size = size;
+    }
+
+    public Point Position => _position;
+    public Size Size => _size;
+
+    public abstract void Draw();
+    public abstract void Update();
+}
