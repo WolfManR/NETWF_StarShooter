4ee785d43284e84d83e17fe59d1b3319b3e8e248
Tarxos <ftarxos@gmail.com>
2022-07-17 22:00:04 +0300

    add assets images paths

--- StarShooter/Configuration.cs
+++ StarShooter/Configuration.cs
@@ -4,4 +4,16 @@ public static class Configuration
 {
     public const int WindowWidth = 800;
     public const int WindowHeight = 600;
+
+    public static class Assets
+    {
+        public const string Star = nameof(Assets) + "/" + "star.png";
+        public const string Ship = nameof(Assets) + "/" + "ship.png";
+        public const string Heal = nameof(Assets) + "/" + "Heal.png";
+        public const string MainMenuBackground = nameof(Assets) + "/" + "BackGround1.jpg";
+        public const string SceneBackground1 = nameof(Assets) + "/" + "BackGround2.jpg";
+        public const string Asteroid1 = nameof(Assets) + "/" + "Asteroid1.png";
+        public const string Asteroid2 = nameof(Assets) + "/" + "Asteroid2.png";
+        public const string Asteroid3 = nameof(Assets) + "/" + "Asteroid3.png";
+    }
 }
